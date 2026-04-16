ab11a3c82c29ef460f27820d1695d474a298b960
Rod5454 <gonzalesrodolfo100@gmail.com>
2026-04-16 14:57:26 -0500

    Agregado al basetest en configuraciones de webdriver y chromeoptions

--- src/test/java/com/example/rosatel/base/BaseTest.java
+++ src/test/java/com/example/rosatel/base/BaseTest.java
@@ -1,5 +1,37 @@
 package com.example.rosatel.base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.Collections;
+import java.time.Duration;
+
 public class BaseTest {
-    
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.setExperimentalOption("useAutomationExtension", false);
+        
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit(); 
+        }
+    }
 }
