b49603fb64697abf898ac3a26748c38493493a9f
Joseph Alexander <110260478+JoeAlexYN18@users.noreply.github.com>
2026-04-17 16:30:56 -0500

    Add complete purchase flow test case

--- Proyecto_Selenium_Rosatel/src/test/java/com/example/rosatel/tests/RosatelFlujoTest.java
+++ Proyecto_Selenium_Rosatel/src/test/java/com/example/rosatel/tests/RosatelFlujoTest.java
@@ -31,4 +31,37 @@ public class RosatelFlujoTest extends BaseTest {
         registroPage.navegarYRegistrarCorreo("gmailprueba@gmail.com");
         
     }
+
+    @Test
+    public void test03_FlujoCompletoDeCompra() throws InterruptedException {
+        System.out.println("--- INICIANDO PRUEBA 3: FLUJO COMPLETO DE COMPRA ---");
+        driver.get("https://www.rosatel.pe/");
+        
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.intentarLogin("gonzalesrodolfo100@gmail.com", "Rodolfo5454");
+        Thread.sleep(3000); 
+        
+        System.out.println("Iniciando proceso de carrito...");
+        CartPage cartPage = new CartPage(driver);
+        cartPage.buscarYAgregarProducto("rosas");
+        
+        cartPage.irAlCheckout();
+        
+        System.out.println("Entregando el control a la página de Checkout...");
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+        checkoutPage.validarIngresoAlCheckout();
+
+        checkoutPage.aceptarTerminosYContinuar();
+        
+        checkoutPage.manejarPopUpDedicatoria();
+
+        checkoutPage.llenarDatosPersonales("Rodolfo", "Gonzales Castillo", "75869160", "970615796");
+        
+        checkoutPage.seleccionarRetiroEnTienda("Rodolfo", "999999999");
+
+        checkoutPage.llenarPagoConDatosDummyYFinalizar();
+
+        System.out.println("PRUEBA FINALIZADA CORRECTAMENTE.");
+        Thread.sleep(20000); 
+    }
 }
