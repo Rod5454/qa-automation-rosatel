d86399900642a5e3e40281cd5968d7b4e4d4286c
Joseph Alexander <110260478+JoeAlexYN18@users.noreply.github.com>
2026-04-17 14:54:48 -0500

    Refactor RosatelFlujoTest to extend BaseTest

--- Proyecto_Selenium_Rosatel/src/test/java/com/example/rosatel/tests/RosatelFlujoTest.java
+++ Proyecto_Selenium_Rosatel/src/test/java/com/example/rosatel/tests/RosatelFlujoTest.java
@@ -1,5 +1,24 @@
 package com.example.rosatel.tests;
 
-public class RosatelFlujoTest {
-    
+import com.example.rosatel.base.BaseTest;
+import com.example.rosatel.pages.LoginPage;
+import com.example.rosatel.pages.RegistroPage; 
+import com.example.rosatel.pages.CartPage; 
+import com.example.rosatel.pages.CheckoutPage; 
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class RosatelFlujoTest extends BaseTest {
+
+    @Test
+    public void test01_LoginNegativo() throws InterruptedException {
+        System.out.println("--- INICIANDO PRUEBA 1: CASO NEGATIVO (LOGIN FALSO) ---");
+        driver.get("https://www.rosatel.pe/");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.intentarLogin("gmailfake@gmail.com", "fake123");
+        Thread.sleep(5000); 
+    }
 }
